54f0dc891d92d01a084ed4a8028291fa3cd3c4b9
srilekha <srilekhachinthala56@gmail.com>
2024-11-25 18:42:34 +0530

    java maven project

---
+++ src/main/java/com/app/demo/Present_project/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.Present_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
